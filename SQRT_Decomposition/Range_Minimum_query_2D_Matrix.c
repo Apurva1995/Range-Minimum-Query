cbc64aefef8a87ea4bce479c7f127ef43b898412
Apurva1995 <42551079+Apurva1995@users.noreply.github.com>
2018-08-31 19:37:55 +0530

    Code for finding min in submatrix

---
+++ SQRT_Decomposition/Range_Minimum_query_2D_Matrix.c
@@ -0,0 +1,250 @@
+/*This code doesn't do input validations and the range for queries as well as index for updation are hard coded.*/
+
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+
+int createFirstLevelMat(int firstSize, int firstLevelSize, int rows, int cols, int firstLevelMat[][firstLevelSize], int matrix[][cols]) {
+    
+    int count;
+    
+    for(int i=0;i<rows;i++) {
+        
+        count = -1;    
+        for(int j=0;j<cols;j++) {
+            
+            if(j%firstSize == 0) {
+                
+                count++;
+                firstLevelMat[i][count] = INT_MAX;
+            }
+            
+            if(firstLevelMat[i][count] > matrix[i][j]) {
+                
+                firstLevelMat[i][count] = matrix[i][j];
+            }
+        }
+    }
+    
+    return count;
+}
+
+int createFinalLevelMat(int secondSize, int firstLevelSize, int secondLevelSize, int rows, int finalFirstLevelSize, int finalSecondLevelSize, int firstLevelMat[][firstLevelSize], int finalLevelMat[][firstLevelSize]) {
+    
+    int count;
+    
+    for(int i=0;i<=finalFirstLevelSize;i++) {
+        
+        count = -1;
+        for(int j=0;j<rows;j++) {
+            
+            if(j % secondSize == 0) {
+                
+                count++;
+                finalLevelMat[count][i] = INT_MAX;
+            }
+            if(finalLevelMat[count][i] > firstLevelMat[j][i]) {
+                
+                finalLevelMat[count][i] = firstLevelMat[j][i];
+            }
+        }
+    }
+    
+    return count;
+}
+
+int getMin(int x1, int x2, int y1, int y2, int firstSize, int secondSize, int firstLevelSize, int cols, int matrix[][cols], int firstLevelMat[][firstLevelSize], int finalLevelMat[][firstLevelSize]) {
+    
+    int min = INT_MAX;
+    int tempY1 = y1;
+    
+    while(x1 % secondSize != 0 && x1 <= x2) {
+        
+        while(tempY1 % firstSize != 0 && tempY1 <= y2) {
+            
+            if(min > matrix[x1][tempY1])
+                min = matrix[x1][tempY1];
+            
+            tempY1++;
+        }
+        
+        while(tempY1 + firstSize-1 <= y2) {
+            
+            if(min > firstLevelMat[x1][tempY1/firstSize])
+                min = firstLevelMat[x1][tempY1/firstSize];
+            
+            tempY1 += firstSize;
+        }
+        
+        while(tempY1 <= y2) {
+            
+            if(min > matrix[x1][tempY1])
+                min = matrix[x1][tempY1];
+            
+            tempY1++;
+        }
+        
+        x1++;
+    }
+    
+    tempY1 = y1;
+    while(x1 + secondSize-1 <= x2) {
+        
+        while(tempY1 % firstSize != 0 && tempY1 <= y2) {
+            
+            int temp = x1;
+            int minTemp = INT_MAX;
+            
+            while(temp <= x2) {
+                
+                if(minTemp > matrix[temp][tempY1])
+                    minTemp = matrix[temp][tempY1];
+                
+                temp++;
+            }
+            
+            if(min > minTemp)
+                min = minTemp;
+            
+            tempY1++;
+        }
+        
+        while(tempY1 + firstSize-1 <= y2) {
+            
+            if(min > finalLevelMat[x1/secondSize][tempY1/firstSize])
+                min = finalLevelMat[x1/secondSize][tempY1/firstSize];
+                
+            tempY1 += firstSize;
+        }
+        
+        while(tempY1 <= y2) {
+            
+            int temp = x1;
+            int minTemp = INT_MAX;
+            
+            while(temp <= x2) {
+                
+                if(minTemp > matrix[temp][tempY1])
+                    minTemp = matrix[temp][tempY1];
+                
+                temp++;
+            }
+            
+            if(min > minTemp)
+                min = minTemp;
+            
+            tempY1++;
+        }
+        
+        x1 += secondSize;
+    }
+    
+    tempY1 = y1;
+    while(x1 <= x2) {
+        
+        while(tempY1 % firstSize != 0 && tempY1 <= y2) {
+            
+            if(min > matrix[x1][tempY1])
+                min = matrix[x1][tempY1];
+            
+            tempY1++;
+        }
+        
+        while(tempY1 + firstSize-1 <= y2) {
+            
+            if(min > firstLevelMat[x1][tempY1/firstSize])
+                min = firstLevelMat[x1][tempY1/firstSize];
+            
+            tempY1 += firstSize;
+        }
+        
+        while(tempY1 <= y2) {
+            
+            if(min > matrix[x1][tempY1])
+                min = matrix[x1][tempY1];
+            
+            tempY1++;
+        }
+        
+        x1++;
+    }
+    
+    return min;
+}
+
+
+void update(int x, int y, int val, int cols, int firstSize, int secondSize, int firstLevelSize, int matrix[][cols], int firstLevelMat[][firstLevelSize], int finalLevelMat[][firstLevelSize]) {
+    
+    matrix[x][y] = val;
+    
+    int start = y/firstSize;
+    start *= firstSize;
+    int min = matrix[x][start];
+    
+    for(int i=start + 1;i< start + firstSize;i++) {
+        
+        if(min > matrix[x][i])
+            min = matrix[x][i];
+    }
+  
+    firstLevelMat[x][y/firstSize] = min;
+        
+    start = x/secondSize;
+    start *= secondSize;
+    min = firstLevelMat[start][y/firstSize];
+    
+    for(int i=start + 1;i< start + secondSize;i++) {
+        
+        if(min > firstLevelMat[i][y/firstSize])
+            min = firstLevelMat[i][y/firstSize];
+    }
+    
+    finalLevelMat[x/secondSize][y/firstSize] = min;
+}
+
+int main(void) {
+	
+	int rows, cols, firstLevelSize, secondLevelSize, firstSize, secondSize, finalFirstLevelSize, finalSecondLevelSize;
+	
+	printf("Enter number of rows and cols\n");
+	scanf("%d", &rows);
+	scanf("%d", &cols);
+	
+	int matrix[rows][cols];
+	
+	printf("Enter values in matrix\n");
+	for(int i=0;i<rows;i++) {
+	    
+	    for(int j=0;j<cols;j++) {
+	        
+	        scanf("%d", &matrix[i][j]);
+	    }
+	}
+	
+	firstSize = sqrt(cols);
+	firstLevelSize = firstSize + 1;
+	
+	int firstLevelMat[rows][firstLevelSize];
+	
+    finalFirstLevelSize = createFirstLevelMat(firstSize, firstLevelSize, rows, cols, firstLevelMat, matrix);
+	
+	secondSize = sqrt(rows);
+    secondLevelSize = secondSize + 1;
+	
+	int finalLevelMat[secondLevelSize][firstLevelSize];
+	
+	finalSecondLevelSize = createFinalLevelMat(secondSize, firstLevelSize, secondLevelSize, rows, finalFirstLevelSize, finalSecondLevelSize, firstLevelMat, finalLevelMat);
+	
+	//Finding minimum in the range x1=0, x2=0, y1=0, y2=1
+	printf("Minimum value present in the given range is : %d\n", getMin(1, 2, 1, 4, firstSize, secondSize, firstLevelSize, cols, matrix, firstLevelMat, finalLevelMat));
+    
+    //Updating value at x=2 and y = 2
+    update(2, 2, 2, cols, firstSize, secondSize, firstLevelSize, matrix, firstLevelMat, finalLevelMat);
+    
+    //Finding minimum in the range x1=0, x2=0, y1=0, y2=1
+	printf("Minimum value present in the given range is : %d ", getMin(1, 2, 1, 4, firstSize, secondSize, firstLevelSize, cols, matrix, firstLevelMat, finalLevelMat));
+    
+	return 0;
+}
+
